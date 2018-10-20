9980fe1920a0919c161f060cdf449c2f54ae7701
Chris Fetz <chrisrfetz@gmail.com>
2018-10-20 17:09:52 -0400

    Code should convert all restaurant csvs to proper format

--- Cleaner.py
+++ Cleaner.py
@@ -4,11 +4,13 @@ import pandas as pd
 countZip = 0
 countCounty = 0
 search = SearchEngine(simple_zipcode=False)
-simpleSearch = SearchEngine(simple_zipcode=True)
 
+#get the zipcode from the 'latitude' and 'longitude' columns in a row
 def getZipcode(row):
     global countZip
     global search
+
+    #print progress so I know how far into the process it is
     countZip+= 1
     string = 'getting zipcode ' + str(countZip)
     print string
@@ -17,6 +19,8 @@ def getZipcode(row):
     longitude = row['longitude']
 
     result = search.by_coordinates(latitude, longitude)
+
+    #if the address isn't an american address, there won't be a zipcode
     if not result:
         print 'None'
         return 'None'
@@ -27,6 +31,8 @@ def getZipcode(row):
 def getCounty(row):
     global countCounty
     global search
+
+    #print progress so I know how far into the process it is
     countCounty+= 1
     string = 'getting county ' + str(countCounty)
     print string
@@ -34,9 +40,11 @@ def getCounty(row):
     zipcode = row['zipcode']
     print zipcode
 
+    #If no zipcode has been found, don't bother trying to find a county
     if zipcode == 'None':
         print 'None'
         return 'None'
+    #Just in case a zipcode doesn't have a corresponding county in the uszipcode database
     else:
         result = search.by_zipcode(zipcode)
         if not result:
@@ -46,27 +54,43 @@ def getCounty(row):
             print result.county
             return result.county
 
-#loop that creates the csvs
-oldpath = 'poi-data/old'
-newpath = 'poi-data/new'
-csvs = {'arbys.csv', 'burgerking.csv', 'carlsjr.csv', 'chickfila.csv', 
+oldpath = 'poi-data/old/'
+newpath = 'poi-data/new/'
+csvs = ['arbys.csv', 'burgerking.csv', 'carlsjr.csv', 'chickfila.csv', 
         'chipotle.csv', 'dominos.csv', 'dq.csv', 'fiveguys.csv', 
-        'hardees.csv', 'jimmyjohns.csv', 'kfc.csv', 'mcdonalds.csv', 
-        'pandaexpress.csv', 'pizzahut.csv', 'sonic.csv', 'subway.csv',
-        'tacobell.csv', 'wendys.csv', 'whataburger.csv', 'jackinthebox.csv'}
+        'hardees.csv', 'jackinthebox.csv', 'jimmyjohns.csv', 'kfc.csv', 
+        'mcdonalds.csv', 'pandaexpress.csv', 'pizzahut.csv', 'sonic.csv', 
+        'subway.csv', 'tacobell.csv', 'wendys.csv', 'whataburger.csv']
 
-#read from the csv file. columns are longitude, latitude, restaurant name, address
-df = pd.read_csv('poi-data/old/Arbys_USA_CAN.csv', names=['longitude', 'latitude', 'title', 'address'])
-df = df.reindex(columns=['latitude', 'longitude'])
-df['name'] = 'Arby\'s'
-df['zipcode'] = df.apply(lambda row: getZipcode(row), axis=1)
-df['county'] = df.apply(lambda row: getCounty(row), axis=1)
-df = df[df.county != 'None']
-print df
+names = ['Arby\'s', 'Burger King', 'Carl\'s Jr.', 'Chick-Fil-A',
+         'Chipotle', 'Domino\'s', 'Dairy Queen', 'Five Guys',
+         'Hardee\'s', 'Jack in the Box', 'Jimmy John\'s', 'KFC',
+         'McDonald\'s', 'Panda Express', 'Pizza Hut', 'Sonic Drive-In',
+         'Subway', 'Taco Bell', 'Wendy\'s', 'Whataburger']
 
-df.to_csv('poi-data/new/arbys.csv', index=False)
+#loop that creates the csvs
+for i in range (0, 20):
+    countZip = 0
+    countCounty = 0
+    oldcsv = oldpath + csvs[i]
 
-#search = SearchEngine(simple_zipcode=False)
-#result = search.by_coordinates(33.949688, -83.399628, radius=30, returns=1)
-#print (result[0].county)
-#print (result[0].zipcode)
+    #read from the csv file. columns are longitude, latitude, restaurant name, address
+    df = pd.read_csv(oldcsv, names=['longitude', 'latitude', 'title', 'address'])
+
+    #we only care about the 'latitude' and 'longitude' columns
+    df = df.reindex(columns=['latitude', 'longitude'])
+    
+    #we want to add name, zipcode and county columns to compare with our taxdata dataset
+    df['name'] = names[i]
+    df['zipcode'] = df.apply(lambda row: getZipcode(row), axis=1)
+    df['county'] = df.apply(lambda row: getCounty(row), axis=1)
+    
+    #we only care about US addresses, so remove any canadian addresses
+    df = df[df.county != 'None']
+    
+    #print the dataframe for debugging purposes
+    print df
+
+    #write result to a csv file in poi-data/new, don't include index column
+    newcsv = newpath + csvs[i]
+    df.to_csv(newcsv, index=False, float_format='%.6f')
