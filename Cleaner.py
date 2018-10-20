51b72a5ea13459281349ad717099f2f5541e4af7
Chris Fetz <chrisrfetz@gmail.com>
2018-10-20 00:25:09 -0400

    Got basic pandas working

--- Cleaner.py
+++ Cleaner.py
@@ -1,10 +1,16 @@
 from uszipcode import SearchEngine
 import pandas as pd
 
-search = SearchEngine(simple_zipcode=False)
-result = search.by_coordinates(33.949688, -83.399628, radius=30, returns=1)
-print (result[0].county)
-print (result[0].zipcode)
+df = pd.read_csv('poi-data/old/Arbys_USA_CAN.csv', names=['latitude', 'longitude', 'title', 'address'])
+smallerDf = df.reindex(columns=['latitude', 'longitude'])
+smallerDf['name'] = 'Arby\'s'
+print smallerDf
+
+#search = SearchEngine(simple_zipcode=False)
+#result = search.by_coordinates(33.949688, -83.399628, radius=30, returns=1)
+#print (result[0].county)
+#print (result[0].zipcode)
+
 
 
 #with open ('<filename>') as csv_file:
